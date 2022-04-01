aa9e700fc58b0367b13f3362bec1ead2a7aefa4f
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-04-01 11:38:56 +0300

    formspree.io

--- js/script.js
+++ js/script.js
@@ -18,3 +18,37 @@
         header__menu_locations.addEventListener('click', ()=> burger());
         header__menu_benefits.addEventListener('click', ()=> burger());
         header__menu_contact.addEventListener('click', ()=> burger());
+
+
+// FORM AJAX.
+
+const form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      const status = document.getElementById("my-form-status");
+      const data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Thanks for your submission!";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! There was a problem submitting your form"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
